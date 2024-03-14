a1db31a8d585b686df0c31f3ce057734a519cae1
Ny Vahatra <rasoloherynyvahatra1@gmail.com>
2024-03-14 11:51:42 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+declare var AOS:any
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,9 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+  ngOnInit(): void {
+      AOS.init({duration:1000})
+  }
 }
